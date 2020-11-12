f7114124f57e2a826581382f5ebe86385eee361d
Stefan Calamaras <72933053+Keenfire901@users.noreply.github.com>
2020-11-12 14:32:06 +0100

    Skapade en kiosk med en meny.

---
+++ test1/src/test1/Kiosk.java
@@ -0,0 +1,16 @@
+package test1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Kiosk {
+	private List<String> meny;
+	
+	public Kiosk() {
+		meny = new ArrayList<String>();
+		meny.add("Korv");
+		meny.add("Hamburgare");
+		meny.add("Läsk");
+	}
+
+}
