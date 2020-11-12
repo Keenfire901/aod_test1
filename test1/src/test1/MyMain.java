0e459f64c1f03c6822c1b1ab54fa5b4764599ba1
Stefan Calamaras <72933053+Keenfire901@users.noreply.github.com>
2020-11-12 14:24:05 +0100

    Nu gör main något

--- test1/src/test1/MyMain.java
+++ test1/src/test1/MyMain.java
@@ -3,7 +3,10 @@ package test1;
 public class MyMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Skriver ut något");
+		if(1==1) {
+			System.out.println("Gör nå basic matte");
+		}
 
 	}
 
